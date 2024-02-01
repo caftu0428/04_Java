a9b8d3b851b63d2920ec851ea857da9014e34f5a
caftu0428 <carcar0428@naver.com>
2024-02-01 17:41:43 +0900

    ool

--- 04_array/src/edu/kh/array/practice/PracticeRun.java
+++ 04_array/src/edu/kh/array/practice/PracticeRun.java
@@ -7,7 +7,17 @@ public class PracticeRun {
 		ArrayPractice ap = new ArrayPractice();
 		
 //		ap.practice1();
-		ap.practice2();
+//		/* (아직) */		ap.practice2();
+//		ap.practice3();
+//		ap.practice4();
+//		ap.practice5();
+//		ap.practice6();
+//		ap.practice7();
+//		ap.practice8();
+//		ap.practice9();
+//		ap.practice10();
+//		ap.practice11();
+//		ap.practice12();
 		
 		
 	}
